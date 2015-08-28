0bab1823f568ddbeac5904439a9450ddfb3ed74b
Junyi Chai <junyic@princeton.edu>
2015-08-28 15:21:15 -0400

    add poisson random number generator compile succesfully

---
+++ src/atmos_spectral_shallow/rand_generator.F90
@@ -0,0 +1,86 @@
+module rand_generator_mod
+
+implicit none
+private
+
+interface gen_rand
+  module procedure gen_rand_int, &
+                   gen_rand_uniform, &
+                   gen_rand_poisson
+end interface
+
+public :: RandInt,               &
+          RandUniform,           &
+          construct_RandPoisson, &
+          RandPoisson
+public :: gen_rand
+
+type RandInt
+    integer(8) :: current_seed_
+end type RandInt
+
+type RandUniform
+    integer(8) :: current_seed_
+end type RandUniform
+
+type RandPoisson
+    integer(8) :: current_seed_
+    real :: exp_neg_lambda_ !exp(-lambda)
+end type RandPoisson
+
+
+contains
+
+!=============================================================================
+
+!@brief Park-Miller random integer generator
+!       Generate a random integer within [1, 2**31-1]
+function gen_rand_int(this) result (random_int)
+  type(RandInt), intent(inout) :: this
+  integer(8) :: random_int
+
+  random_int = mod(48271_8*this%current_seed_, 2147482647_8)
+  this%current_seed_ = random_int
+end function gen_rand_int
+
+!@brief generate uniform random number within (0, 1)
+function gen_rand_uniform(this) result (random_real)
+  type(RandUniform), intent(inout) :: this
+  real :: random_real
+
+  this%current_seed_ = mod(48271_8*this%current_seed_, 2147482647_8)
+  random_real = (1.0/2147482648.0)*this%current_seed_
+end function gen_rand_uniform
+
+!@brief constructor for generating a Poisson random number with distribution
+!       lambda**k * exp(-lambda) / k!
+function construct_RandPoisson(seed, lambda) result (this)
+  integer, intent(in) :: seed
+  real :: lambda
+  type(RandPoisson) :: this
+
+  this%current_seed_ = seed
+  this%exp_neg_lambda_ = exp(-lambda)
+end function construct_RandPoisson
+
+!@brief see algorithm by D.P. Kroese, T. Taimre, Z.I. Botev:
+!       Handbook of Monte Carlo Methods. John Wiley & Sons, 2011.
+function gen_rand_poisson(this) result(random_poisson)
+  type(RandPoisson), intent(inout) :: this
+  integer :: random_poisson
+  real :: random_real, produ
+
+  random_poisson = 0
+  random_real = (1.0/2147482648.0)*this%current_seed_
+  this%current_seed_ = mod(48271_8*this%current_seed_, 2147482647_8)
+  produ = random_real
+
+  do while (produ >= (this%exp_neg_lambda_))
+    random_real = (1.0/2147482648.0)*this%current_seed_
+    this%current_seed_ = mod(48271_8*this%current_seed_, 2147482647_8)
+    produ = produ * random_real
+    random_poisson = random_poisson + 1
+  enddo
+end function gen_rand_poisson
+
+end module rand_generator_mod
